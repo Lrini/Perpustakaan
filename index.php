8e41ae3d3b8ac88462c5089b77446c6ef782c4ee
Lrini <iwangzr94@gmail.com>
2021-03-29 13:09:37 +0700

    revisi limabelas

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Login E-libarary</title>
+  </head>
+  <body>
+  
+
+  
+  <div class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="images/college.png" width="400px" height="400px" alt="Image" class="img-fluid">
+        </div>
+        <div class="col-md-6 contents">
+          <div class="row justify-content-center">
+            <div class="col-md-8">
+              <div class="mb-4">
+              <h3>Sign In</h3>
+              <p class="mb-4">E-libarary diperuntukan untuk peserta didik LP3I COLLEGE Kupang.</p>
+            </div>
+            <?php
+                    if(isset($_GET['pesan'])){
+                    if($_GET['pesan'] == "gagal"){
+                    echo "Login gagal! username dan password salah!";
+                    }else if($_GET['pesan'] == "logout"){
+                    echo "Anda telah berhasil logout";
+                    }else if($_GET['pesan'] == "belum_login"){
+                    echo "Anda harus login terlebih dahulu";
+                    }
+                    }
+                    ?>
+            <form action="login.php" method="post">
+              <div class="form-group first">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username">
+
+              </div>
+              <div class="form-group last mb-4">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+                
+              </div>
+
+              <input type="submit" value="Log In" class="btn btn-block btn-primary">
+            </form>
+            </div>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+  
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
